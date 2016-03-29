3bbb0446b500ed3206f49599c253f2f28a71b79c
dbishop <dbishop.rwc@gmail.com>
2016-03-29 17:02:57 -0400

    switched physics engine to matter.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+"use strict"
 var express = require("express");
 var path = require("path");
 var http = require("http");
 var socketio = require("socket.io");
 var routes = require("./src/routes");
 var sockets = require("./src/sockets");
-var world = require("./src/world");
-
+var World = require("./src/world");
+var Matter = require("./lib/matter.js");
 var app = express();
 var server = http.createServer(app);
 var io = socketio.listen(server);
@@ -18,7 +19,22 @@ app.get("/", routes.home);
 
 sockets.bind(io);
 
-world.start();
+var boxA = Matter.Bodies.rectangle(400, 200, 80, 80);
+var boxB = Matter.Bodies.rectangle(450, 50, 80, 80);
+
+var world1 = new World();
+var world2 = new World();
+
+console.log("Box A Position", boxA.position);
+console.log("Box B Position", boxB.position);
+world1.add([boxA]);
+world2.add([boxB]);
+world1.step(100);
+console.log("Box A Position", boxA.position);
+console.log("Box B Position", boxB.position);
+world2.step(100);
+console.log("Box A Position", boxA.position);
+console.log("Box B Position", boxB.position);
 
 server.listen(PORT, function () {
   console.log("Application running on port:", PORT);
--- src/world.js
+++ src/world.js
@@ -1,56 +1,43 @@
-var P = require('../lib/physicsjs/physicsjs-full.js');
+var Matter = require("../lib/matter.js");
+// var engine = Matter.Engine.create();
+// var engine2 = Matter.Engine.create();
 
-P(function (world) {
-  P.util.ticker.tickrate(64); // Ticks/second
-  world.add(P.behavior('constant-acceleration'), {
-    acc: { x : 0, y: 0.0004 } // Standard gravity
-  });
-  world.add(P.behavior('body-collision-detection'));
-  world.add(P.behavior('sweep-prune'));
-  world.add(P.behavior('body-impulse-response'));
+// var boxA = Matter.Bodies.rectangle(400, 200, 80, 80);
+// var boxB = Matter.Bodies.rectangle(450, 50, 80, 80);
+// var ground = Matter.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-  // Fire world step event on ticker step
-  P.util.ticker.on(function (time, dt) {
-    world.step(time);
-  });
+// Matter.World.add(engine.world, [boxA, ground]);
+// Matter.World.add(engine2.world, [boxB, ground]);
 
-  // Function callback on physics engine step
-  world.on('step', function(){
-    
-  });
+// console.log('boxA', boxA.position);
+// console.log('boxB', boxB.position);
 
-  /*
-    loadStage() -> null
-    Clear the world, add a set of objects to the world
-  */
-  function loadScene (stage) {
-    world.remove(world.getBodies());
-    world.add(stage.bodies);
+// for (var i = 0; i < 100; i++) {
+//     Matter.Events.trigger(engine, 'tick', { timestamp: engine.timing.timestamp });
+//     Matter.Engine.update(engine, engine.timing.delta);
+//     Matter.Events.trigger(engine, 'afterTick', { timestamp: engine.timing.timestamp });
+// }
+
+// console.log('boxA', boxA.position);
+// console.log('boxB', boxB.position);
+
+function World() {
+  this.engine = Matter.Engine.create();
+
+  this.add = function(bodies) {
+    Matter.World.add(this.engine.world, bodies);
   }
 
-  world.fireArrow = function (pos, vel) {
-    world.add(P.body('convex-polygon', {
-      x: pos.x,
-      y: pos.y,
-      vx: vel.x,
-      vy: vel.y,
-      vertices: [
-        {x: -10, y: 0},
-        {x: -7, y: 2},
-        {x: -7, y: 1},
-        {x: 10, y: 1},
-        {x: 10, y: -1},
-        {x: -7, y: -1},
-        {x: -7, y: -2},
-      ]
-    }));
+  this.step = function(steps) {
+    if (!steps) {
+      steps = 1;
+    }
+    for (var i = 0; i < steps; i++) {
+      Matter.Events.trigger(this.engine, 'tick', { timestamp: this.engine.timing.timestamp });
+      Matter.Engine.update(this.engine, this.engine.timing.delta);
+      Matter.Events.trigger(this.engine, 'afterTick', { timestamp: this.engine.timing.timestamp });
+    }
   }
+}
 
-  // API
-  module.exports = {
-    start: P.util.ticker.start,
-    stop: P.util.ticker.stop,
-    scene: world,
-    loadScene: loadScene
-  }
-});
+module.exports = World;
