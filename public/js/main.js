7ca415e62acd8d4ca7c0501f489201b08ce3f956
Joey L Maalouf <joeylmaalouf@gmail.com>
2016-04-01 21:30:31 +0000

    more detailed events

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,61 @@
 var socket = io.connect();
-var gameIDText = $("#gameIDText");
-var createGameText = $("#createGameText");
-var joinGameText = $("#joinGameText");
+var gameIDText, createGameText, joinGameText;
+var createGameButton, joinGameButton, endGameButton;
 var gameID;
 
-socket.on("set game id", function (data) {
-  gameID = data.gameID;
-  gameIDText.text(gameID);
+$(document).ready(function () {
+  gameIDText = $("#gameIDText");
+  createGameText = $("#createGameText");
+  joinGameText = $("#joinGameText");
+  createGameButton = $("#createGameButton");
+  joinGameButton = $("#joinGameButton");
+  endGameButton = $("#endGameButton");
+  
+  createGameButton.click(createGame);
+  joinGameButton.click(joinGame);
+  endGameButton.click(endGame);
+  
+  inLobby();
 });
 
 var createGame = function () {
-  endGame();
   socket.emit("create game", {});
-  createGameText.text("Waiting for player 2...");
 };
 
 var joinGame = function () {
-  endGame();
   socket.emit("join game", {
     gameID: joinGameText.val()
   });
-  createGameText.text("Waiting for player 1...");
   joinGameText.val("");
 };
 
 var endGame = function () {
   socket.emit("end game", {});
+};
+
+var inLobby = function (data) {
   gameID = null;
   gameIDText.text("You are not currently in a game.");
   createGameText.text("");
+  createGameButton.show();
+  joinGameButton.show();
+  joinGameText.show();
+  endGameButton.hide();
 };
+
+var inWaiting = function (data) {
+  gameID = data.gameID;
+  gameIDText.text(gameID);
+  createGameText.text("Waiting for player " + data.otherPlayer + "...");
+  createGameButton.hide();
+  joinGameButton.hide();
+  joinGameText.hide();
+  endGameButton.show();
+};
+
+socket.on("create game success", inWaiting);
+socket.on("join game success", inWaiting);
+socket.on("end game success", inLobby);
+
+socket.on("join game failure", function (data) {
+});
--- src/sockets.js
+++ src/sockets.js
@@ -15,29 +15,51 @@ var bindSockets = function (io) {
     socket.on("create game", function (data) {
       var game = coordinator.createGame(socket);
       socket.game = game;
-      socket.emit("set game id", {
-        gameID: socket.game.id
-      });
+      socket.player = "1";
+      if (game) {
+        socket.emit("create game success", {
+          gameID: socket.game.id,
+          otherPlayer: "2"
+        });
+      }
+      else {
+        socket.emit("create game failure", {});
+      }
     });
 
     socket.on("join game", function (data) {
       var game = coordinator.joinGame(socket, data.gameID);
       socket.game = game;
+      socket.player = "2";
       if (game) {
-        socket.emit("set game id", {
-          gameID: socket.game.id
+        game.p1.emit("join game success", {
+          gameID: socket.game.id,
+          otherPlayer: p2.player
         });
-        game.p1.emit("join game success", {/*TODO*/});
-        game.p2.emit("join game success", {/*TODO*/});
+        game.p2.emit("join game success", {
+          gameID: socket.game.id,
+          otherPlayer: p1.player
+        });
       }
       else {
-        socket.emit("join game failure", {/*TODO*/});
+        socket.emit("join game failure", {});
       }
     });
 
     socket.on("end game", function (data) {
       if (socket.game) {
         coordinator.endGame(socket.game.id);
+        if (socket.game.p1) {
+          socket.game.p1.emit("end game success", {});
+          delete socket.game.p1.game;
+        }
+        if (socket.game.p2) {
+          socket.game.p2.emit("end game success", {});
+          delete socket.game.p2.game;
+        }
+      }
+      else {
+        socket.emit("end game failure", {});
       }
     });
   });
