f831db09846cfaf73c5eddc49a55d760d0f6e8a1
Joey L Maalouf <joeylmaalouf@gmail.com>
2016-04-08 21:13:38 +0000

    QoL style stuff

--- src/coordinator.js
+++ src/coordinator.js
@@ -1,5 +1,5 @@
 var shortid = require("shortid");
-shortid.characters("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@");
+shortid.characters("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$_");
 
 var games = {};
 
